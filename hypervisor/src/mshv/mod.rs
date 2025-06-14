jinankjain/cloud-hypervisor

commit 59e11f1b0bb256d616fc16d8803d60498add4e8d

hypervisor: mshv: fix advance_rip_update_rax() helper
The dirty bit for the GP registers must be set for the hypervisor to
update them.

Signed-off-by: Nuno Das Neves <nunodasneves@linux.microsoft.com>

hypervisor/src/mshv/mod.rs
@@ -1711,10 +1711,11 @@ impl MshvVcpu {
         if let Some(reg_page) = self.fd.get_vp_reg_page() {
             let vp_reg_page = reg_page.0;
             set_gp_regs_field_ptr!(vp_reg_page, rax, ret_rax);
-            // SAFETY: access union fields
+            // SAFETY: access raw pointer to reg page, access union fields
             unsafe {
                 (*vp_reg_page).__bindgen_anon_1.__bindgen_anon_1.rip = info.header.rip + insn_len;
                 (*vp_reg_page).dirty |= 1 << HV_X64_REGISTER_CLASS_IP;
+                (*vp_reg_page).dirty |= 1 << HV_X64_REGISTER_CLASS_GENERAL;
             }
         } else {
             let arr_reg_name_value = [

commit e96e787c030f043e29678f54e6ece0652fdf6022

hypervisor: mshv: Fix the ordering of marking regs as dirty
First we are updating general purpose register rax, thus we should mark
it as dirty before we mark instruction pointer as dirty inside the VP
register page. Due to this incorrect ordering, noticed that guest is
stuck inside systemd and could not progress with the boot further.A

Fixes: 59e11f1b0 (hypervisor: mshv: fix advance_rip_update_rax() helper)
Fixes: f67484a71 (hypervisor: mshv: advance_rip_rax after port handle)
Signed-off-by: Jinank Jain <jinankjain@microsoft.com>

hypervisor/src/mshv/mod.rs
@@ -1713,9 +1713,9 @@ impl MshvVcpu {
             set_gp_regs_field_ptr!(vp_reg_page, rax, ret_rax);
             // SAFETY: access raw pointer to reg page, access union fields
             unsafe {
+                (*vp_reg_page).dirty |= 1 << HV_X64_REGISTER_CLASS_GENERAL;
                 (*vp_reg_page).__bindgen_anon_1.__bindgen_anon_1.rip = info.header.rip + insn_len;
                 (*vp_reg_page).dirty |= 1 << HV_X64_REGISTER_CLASS_IP;
-                (*vp_reg_page).dirty |= 1 << HV_X64_REGISTER_CLASS_GENERAL;
             }
         } else {
             let arr_reg_name_value = [